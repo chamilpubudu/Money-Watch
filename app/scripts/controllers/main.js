0e3ab624f38c185329c5183e0b0ad34f8aef86a0
chamilpubudu <chamilpubudu@gmail.com>
2014-10-28 12:36:39 +0530

    initial file commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moneyWatchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the moneyWatchApp
+ */
+angular.module('moneyWatchApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
