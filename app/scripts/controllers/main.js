f01167647076c529d92179450d101d7b1e3071f0
chamilpubudu <chamilpubudu@gmail.com>
2014-10-28 14:13:24 +0530

    Move existing project files

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,34 @@
  */
 angular.module('moneyWatchApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+      $scope.transactions = [{ title: 'Bus ticket', amount: 1000 }, { title: 'Stationary', amount: 1250 }, { title: 'Food', amount: 5000 }];
+      $scope.transaction = null;
+
+      $scope.addTransaction = function () {
+          $scope.transactions.push($scope.transaction);
+          $scope.transaction = null;
+      };
+      $scope.removeTransaction = function (index) {
+          $scope.transactions.splice(index, 1);
+      };
   });
+
+angular.module('moneyWatchApp').directive('onlyDigits', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(val) {
+                if (val == undefined)
+                    val = '';
+                var transformedInput = val.replace(/[^0-9]/g, '');
+                console.log(transformedInput);
+                if (transformedInput !== val) {
+                    ngModelCtrl.$setViewValue(transformedInput);
+                    ngModelCtrl.$render();
+                }
+                return transformedInput;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+});
